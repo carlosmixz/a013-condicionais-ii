20f7bf09c45e105e56f93302ade2a18bd32c95aa
carlosmixz <carlosmixz3@gmail.com>
2022-08-09 18:35:52 -0300

    Disneyworld

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,8 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite seu numero")) //seu numero aqui
+
+if (num % 2 == 0 || num % 3 == 0) {
+    alert("O numero é divisivel por 2 ou 3")
+} else {
+    alert("O numero não é divisivel por 2 ou 3")
+}
 
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,23 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+switch (nacionalidade) {
+    case "brasileira":
+        alert("Brasileira")
+        break;
+    case "argentina":
+        alert("Argentina")
+        break;
+    case "uruguaia":
+        alert("Uruguaia")
+        break;
+    case "chilena":
+        alert("Chilena")
+        break;
+    case "colombiana":
+        alert("Colombiana")
+        break;
+    default:
+        alert("nacionalidade não encontrada")
 }
+
+
